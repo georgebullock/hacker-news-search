94a0be39e892b12f1718deff448e2291c8214a02
George <george.bullock@gmx.de>
2020-07-31 15:24:58 +0200

    Add static monolith component scaffold

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      commentsCount: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov and Andrew Clark",
+      commentsCount: 2,
+      points: 5,
+      objectId: 1,
+    },
+  ];
+
+  let [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem("search") || "React"
+  );
+
+  useEffect(() => localStorage.setItem("search", searchTerm), [searchTerm]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filterStories = stories.filter((story) => {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hacker Stories</h1>
+      <Search handleSearch={handleSearch} searchTerm={searchTerm} />
+      <hr />
+      <List list={filterStories} />
     </div>
   );
-}
+};
+
+const Search = ({ handleSearch, searchTerm }) => {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input
+        name="search"
+        id="search"
+        type="text"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <p>
+        Searching for: <strong>{searchTerm}</strong>
+      </p>
+    </>
+  );
+};
+
+const List = ({ list }) => {
+  const items = list.map((item) => {
+    return <Item key={item.objectId} item={item} />;
+  });
+
+  return <ul>{items}</ul>;
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <p>Title: {item.title}</p>
+      <p>Link: {item.url}</p>
+      <p>Author: {item.author}</p>
+      <p>Comments: {item.commentsCount}</p>
+      <p>Points: {item.points}</p>
+    </li>
+  );
+};
 
 export default App;
