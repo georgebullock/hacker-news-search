ad2c6dd013391ba9a8f909a6f24741a1272fa5ce
George <george.bullock@gmx.de>
2020-10-09 20:07:13 +0200

    Move headline and search back into App.

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Stories from "./../../shared/Stories/Stories";
+import SearchForm from "./../../shared/SearchForm/SearchForm";
+import { useWebStorage } from "./../../shared/hooks/useWebStorage";
 import styles from "./App.module.scss";
 
-/***************************/
-/*        Main App        */
-/*************************/
-
 const App = () => {
+  const [searchTerm, setSearchTerm] = useWebStorage("search", "");
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const FETCH_URL = "https://hn.algolia.com/api/v1/search?query=";
+
+  const [url, setUrl] = useState(`${FETCH_URL}${searchTerm}`);
+
+  const handleSearchSubmit = (e) => {
+    setUrl(`${FETCH_URL}${searchTerm}`);
+    e.preventDefault();
+  };
+
   return (
     <main className={styles.container}>
-      <Stories />
+      <header className={styles.header}>
+        <h1 className={styles.headlineMain}>Hacker News Search</h1>
+        <SearchForm
+          handleSearch={handleSearch}
+          searchTerm={searchTerm}
+          searchSubmit={handleSearchSubmit}
+        />
+      </header>
+      <Stories url={url} />
     </main>
   );
 };
