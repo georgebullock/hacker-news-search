7011dbfc60224ec487b5a98b7775dce97bf3673e
George <george.bullock@gmx.de>
2020-10-11 14:13:02 +0200

    Remove React import

--- src/shared/hooks/useWebStorage.js
+++ src/shared/hooks/useWebStorage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const useWebStorage = (key, initialState) => {
   let [value, setValue] = useState(localStorage.getItem(key) || initialState);
